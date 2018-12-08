c063e5377e4d79760af36e6fd3b7452a9ce8a676
drmmx <m.daglarov@gmail.com>
2018-12-08 14:09:50 +0200

    add pagination

--- app/src/main/java/com/drmmx/devmax/randomuserapp/ui/MainActivity.java
+++ app/src/main/java/com/drmmx/devmax/randomuserapp/ui/MainActivity.java
@@ -1,5 +1,6 @@
 package com.drmmx.devmax.randomuserapp.ui;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
@@ -40,6 +41,10 @@ public class MainActivity extends AppCompatActivity {
     private int pageNumber = 1;
     private int itemCount = 20;
 
+    //Pagination variables
+    private boolean isLoading = true;
+    private int previousTotalItemCount = 0;
+
     //RxJava
     CompositeDisposable compositeDisposable = new CompositeDisposable();
 
@@ -64,6 +69,16 @@ public class MainActivity extends AppCompatActivity {
 
         //Load data
         fetchData();
+
+        //Load pagination
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+
+                loadPagination(recyclerView, dx, dy);
+            }
+        });
     }
 
     private void fetchData() {
@@ -75,6 +90,66 @@ public class MainActivity extends AppCompatActivity {
                     public void accept(RandomUser users) {
                         adapter = new UserAdapter(MainActivity.this, users.getResults());
                         recyclerView.setAdapter(adapter);
+                        progressBar.setVisibility(View.GONE);
+                    }
+                }, new Consumer<Throwable>() {
+                    @Override
+                    public void accept(Throwable throwable) {
+                        Log.d(TAG, "error: " + throwable.getMessage());
+                    }
+                }));
+    }
+
+    private void loadPagination(RecyclerView recyclerView, int dx, int dy) {
+        int firstVisibleItems;
+        int visibleItemCount;
+        int totalItemCount;
+        if (!isGridLayoutEnabled) {
+            firstVisibleItems = linearLayoutManager.findFirstVisibleItemPosition();
+            visibleItemCount = linearLayoutManager.getChildCount();
+            totalItemCount = linearLayoutManager.getItemCount();
+
+            if (dy > 0) {
+                if (isLoading) {
+                    if (totalItemCount > previousTotalItemCount) {
+                        isLoading = false;
+                        previousTotalItemCount = totalItemCount;
+                    }
+                } else if (totalItemCount - visibleItemCount <= firstVisibleItems + itemCount) {
+                    pageNumber++;
+                    loadPageData();
+                    isLoading = true;
+                }
+            }
+        } else {
+            firstVisibleItems = gridLayoutManager.findFirstVisibleItemPosition();
+            visibleItemCount = gridLayoutManager.getChildCount();
+            totalItemCount = gridLayoutManager.getItemCount();
+
+            if (dy > 0) {
+                if (isLoading) {
+                    if (totalItemCount > previousTotalItemCount) {
+                        isLoading = false;
+                        previousTotalItemCount = totalItemCount;
+                    }
+                } else if (totalItemCount - visibleItemCount <= firstVisibleItems + itemCount) {
+                    pageNumber++;
+                    loadPageData();
+                    isLoading = true;
+                }
+            }
+        }
+    }
+
+    private void loadPageData() {
+        progressBar.setVisibility(View.VISIBLE);
+        compositeDisposable.add(userAPI.getUsers("axon", "gender,name,dob,phone,email,picture", pageNumber, itemCount)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<RandomUser>() {
+                    @Override
+                    public void accept(RandomUser users) {
+                        adapter.addUsers(users.getResults());
                         progressBar.setVisibility(View.GONE);
                     }
                 }, new Consumer<Throwable>() {
@@ -117,7 +192,7 @@ public class MainActivity extends AppCompatActivity {
         super.onStop();
     }
 
-    /*@Override
+    @Override
     protected void onResume() {
         super.onResume();
         if (!isGridLayoutEnabled) {
@@ -125,5 +200,5 @@ public class MainActivity extends AppCompatActivity {
         } else {
             recyclerView.setLayoutManager(gridLayoutManager);
         }
-    }*/
+    }
 }
